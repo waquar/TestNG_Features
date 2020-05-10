c39eeb5305d72decb2dfd3852ceeb35b0991456e
waquar <waquar.alam92@gmail.com>
2020-05-11 00:54:30 +0530

    added dataprovider

--- src/Datadriven/ReadDataprovider.java
+++ src/Datadriven/ReadDataprovider.java
@@ -11,22 +11,28 @@ public class ReadDataprovider {
 	
 	
   @Test(dataProvider = "getdata")
-  public void login(String name, String email, String password) throws InterruptedException {
+  public void login(String name, String email, String password, String confirmpassword) throws InterruptedException {
 	  
-	  System.setProperty("webdriver.chrome.driver", "/Users/waquaralam/Automation_Addons/chromedriver");
-	  WebDriver driver = new ChromeDriver();
-	  driver.get("http://sdettraining.com/trguitransactions/NewAccount.aspx");
-	  
-	  Thread.sleep(4000);
-	  driver.findElement(By.xpath("//input[@id='MainContent_txtFirstName']")).sendKeys(name);;
-	  driver.findElement(By.xpath("//input[@id='//input[@id='MainContent_txtEmail']']")).sendKeys(email);;
-      driver.findElement(By.xpath("//input[@id='MainContent_txtPassword']")).sendKeys(password);;
-      System.out.println(driver.getTitle());
+//	  System.setProperty("webdriver.chrome.driver", "/Users/waquaralam/Automation_Addons/chromedriver");
+//	  WebDriver driver = new ChromeDriver();
+//	  driver.get("https://learn.letskodeit.com/");
+//	  
+//	  Thread.sleep(4000);
+//	  driver.findElement(By.xpath("//a[@id='header-sign-up-btn']")).click();
+//	  Thread.sleep(4000);
+//	  driver.findElement(By.xpath("input[@id='user_name']")).sendKeys(name);
+//	  driver.findElement(By.xpath("input[@id='user_email']")).sendKeys(email);
+//      driver.findElement(By.xpath("input[@id='user_password']")).sendKeys(password);
+//      driver.findElement(By.xpath("input[@id='user_password_confirmation']")).sendKeys(confirmpassword);
+//      Thread.sleep(2000);
+      
+      System.out.println(name + email+  password+ confirmpassword);
 	   
   }
+  
   @DataProvider
   public String[][] getdata(){
-	  return Utilities.Excel.get("/Users/waquaralam/Desktop/original.xls");
+	  return Utilities.Excel.get("/Users/waquaralam/Desktop/emailexcel.xls");
 	  
   }
   
